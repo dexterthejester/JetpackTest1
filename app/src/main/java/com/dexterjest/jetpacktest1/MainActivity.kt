099abec2d46bdad0b5f071f3a263348397170cab
DexterJest <d3xter101@gmail.com>
2023-04-30 11:37:07 +0800

    Very minor changes

--- app/src/main/java/com/dexterjest/jetpacktest1/MainActivity.kt
+++ app/src/main/java/com/dexterjest/jetpacktest1/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import com.dexterjest.jetpacktest1.ui.theme.JetpackTest1Theme
 
 class MainActivity : ComponentActivity() {
